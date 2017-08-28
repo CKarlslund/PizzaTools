4e00fce96bf9cf6ad66bb4ceac5e083d649edeef
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-08-28 16:12:46 +0200

    Quantity updates (and issues revealed)

---
+++ Pizzeria/Commands/UpdateQuantityHomeControllerCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pizzeria.Commands
+{
+    public class UpdateQuantityHomeControllerCommand : BaseHomeControllerCommand
+    {
+        public async override Task<IActionResult> Execute(object id)
+        {
+            return Controller.RedirectToAction("Index");
+        }
+    }
+}
