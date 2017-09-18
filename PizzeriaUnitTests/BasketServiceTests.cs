7f3ff90b845c65fd65c978b5971a34f5442c3025
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-09-18 14:12:15 +0200

    Basket service tests finished

--- PizzeriaUnitTests/BasketServiceTests.cs
+++ PizzeriaUnitTests/BasketServiceTests.cs
@@ -3,32 +3,37 @@ using Microsoft.AspNetCore.Http;
 using Pizzeria.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Pizzeria.Data;
+using Pizzeria.Services;
 using Xunit;
 
 namespace PizzeriaUnitTests
 {
-    internal class BasketServiceTests : BasePizzeriaTests
+    public class BasketServiceTests : BasePizzeriaTests
     {
+        private ApplicationDbContext _context;
+
         public override void InitializeDatabase()
         {
             base.InitializeDatabase();
             var context = ServiceProvider.GetService<ApplicationDbContext>();
 
             CreateData(context);
+            _context = context;
         }
 
         private void CreateData(ApplicationDbContext context)
         {
             var dish = new Dish()
             {
-                Price = 70
+                DishId = 33
             };
             context.Dishes.Add(dish);
 
             var basketItem = new BasketItem()
             {
                 Dish = dish,
-                Quantity = 2
+                Quantity = 2,
+                Price = 70
             };
             context.BasketItems.Add(basketItem);
 
@@ -46,29 +51,35 @@ namespace PizzeriaUnitTests
         [Fact]
         public void GetCurrentBasket_returns_correct_basket()
         {
-            ////Arrange
-            //ISession session = new TestSession();
-            //session.SetInt32("BasketId", 3);
+            //Arrange
+            var httpContext = new DefaultHttpContext();
+            httpContext.Session = new TestSession();
+            httpContext.Session.SetInt32("BasketId", 3);
 
-            //////Act
-            //var result = Pizzeria.Services.BasketService.GetCurrentBasket(session);
+            var basketService = new BasketService(_context);
 
-            ////Assert
-            //Assert.Equal(result.BasketId, 3);
+            //Act
+            var result = basketService.GetCurrentBasket(httpContext.Session);
+
+            //Assert
+            Assert.Equal(3, result.BasketId);
         }
         
         [Fact]
         public void GetTotal_returns_correct_total()
         {
-            ////Arrange
-            //ISession session = new TestSession();
-            //session.SetInt32("BasketId", 3);
+            //Arrange
+            var httpContext = new DefaultHttpContext();
+            httpContext.Session = new TestSession();
+            httpContext.Session.SetInt32("BasketId", 3);
 
-            ////Act
-            //var result = Pizzeria.Services.BasketService.GetTotal(session);
+            var basketService = new BasketService(_context);
 
-            ////Assert
-            //Assert.Equal(result, 140);
+            //Act
+            var result = basketService.GetTotal(httpContext.Session);
+
+            //Assert
+            Assert.Equal(140, result);
         }
     }
 }
