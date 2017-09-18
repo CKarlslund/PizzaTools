cfc8da8c707563cfa11340a5645736db8433e2f6
Cornelia Karlslund <cornelia.karlslund@gmail.com>
2017-09-18 15:26:44 +0200

    Add dish controller test

--- PizzeriaUnitTests/DishControllerTests.cs
+++ PizzeriaUnitTests/DishControllerTests.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InMemTests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Pizzeria.Controllers;
@@ -15,24 +17,45 @@ namespace PizzeriaUnitTests
 {
     public class DishControllerTests : BasePizzeriaTests
     {
+        private ApplicationDbContext _context;
+
+        public override void InitializeDatabase()
+        {
+            base.InitializeDatabase();
+            var context = ServiceProvider.GetService<ApplicationDbContext>();
+
+            CreateData(context);
+            _context = context;                      
+        }
+
+        private void CreateData(ApplicationDbContext context)
+        {
+            var dishes = new List<Dish>()
+            {
+                new Dish() {DishId = 1, Name = "Hawaii", CategoryId = 1},
+                new Dish() {DishId = 2, Name = "Vezuvio"},
+                new Dish() {DishId = 3, Name = "Vegetariana"},
+            };
+
+            context.Dishes.AddRange(dishes);
+
+            var category = new Category()
+            {
+                CategoryId = 1
+            };
+
+            context.Categories.Add(category);
+
+            context.SaveChanges();
+        }
+
         [Fact]
         public async Task Returns_correct_dishes()
         {
             //Arrange         
-            var fakeDishes = new List<Dish>()
-            {
-                new Dish(){DishId = 1, Name = "Hawaii"},
-                new Dish(){DishId = 2, Name = "Vezuvio"},
-                new Dish(){DishId = 3, Name = "Vegetariana"},
-            }.ToAsyncDbSetMock();
-
-            var mockDbContext = new Mock<ApplicationDbContext>();
-            mockDbContext.Setup(repo => repo.Dishes).Returns(fakeDishes.Object);
-
             var mockLogger = new Mock<ILogger<DishController>>();
 
-
-            var controller= new DishController(mockDbContext.Object, mockLogger.Object);
+            var controller= new DishController(_context, mockLogger.Object);
 
             //Act
             var result = await controller.Index();
@@ -42,6 +65,30 @@ namespace PizzeriaUnitTests
             var model = Assert.IsAssignableFrom<IEnumerable<Dish>>(
                 viewResult.ViewData.Model);
             Assert.Equal(3, model.Count());
+        }
+
+        [Fact]
+        public async Task Details_ReturnsDishDetail()
+        {
+            // Arrange
+            var dishId = 1;
+
+            var mockLogger = new Mock<ILogger<DishController>>();
+
+            var controller = new DishController(_context, mockLogger.Object);
+
+            // Act
+            var result = await controller.Details(dishId);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.ViewName);
+
+            Assert.NotNull(viewResult.ViewData);
+            var viewModel = Assert.IsType<Dish>(viewResult.ViewData.Model);
+            Assert.NotNull(viewModel.Category);
+            var category = _context.Categories.SingleOrDefault(g => g.CategoryId == viewModel.CategoryId);
+            Assert.NotNull(category);
         }
     }
 }
